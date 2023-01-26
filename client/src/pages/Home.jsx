94d872dbca0f31977701ac791a2aada7479e5d26
shubham_wadne <kaylekashubham@gmail.com>
2023-01-26 18:04:40 +0530

    changed the backend api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             setLoading(true)
 
             try {
-                const response = await fetch('http://localhost:8000/api/v1/post', {
+                const response = await fetch('https://ai-image-app.onrender.com/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
